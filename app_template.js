d8025178437423f3dc952c05c30c085e80a55412
Apisit Ritreungroj <apisit_lon@hotmail.com>
2021-01-12 13:54:23 +0700

    Done

--- app_template.js
+++ app_template.js
@@ -1,1 +1,41 @@
 //your code here!
+
+const myId = 620610820;
+const idInput = document.getElementById("input-number");
+const idBtn = document.getElementById("button-compute");
+const studentId = document.getElementById("compute-result");
+
+const tRowInput = document.getElementById("input-row");
+const tColInput = document.getElementById("input-col");
+const tBtn = document.getElementById("button-create-table");
+const table = document.getElementById("table");
+
+idBtn.addEventListener("click", () => {
+    studentId.textContent = myId + (idInput.valueAsNumber || 0);
+});
+
+tBtn.addEventListener("click", () => {
+    const row = tRowInput.valueAsNumber || 0;
+    const col = tColInput.valueAsNumber || 0;
+
+    let z = 1;
+    for (let i = 0; i < row; i++) {
+        const r = document.createElement("tr");
+
+        for (let j = 0; j < col; j++) {
+            const d = document.createElement("td");
+
+            if ((i + j) % 2 === 0) {
+                d.textContent = "*";
+            }
+            else {
+                d.textContent = z;
+            }
+
+            z++;
+            r.append(d);
+        }
+
+        table.append(r);
+    }
+});
